49d291f2f9e91eb6508d7997932ebad78b43617c
Nicholas Powers <njpowers7915@gmail.com>
2018-07-02 19:05:56 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,14 @@
 class ArtistsController < ApplicationController
   def index
+      @artists = Artist.all
   end
 
   def show
+      @artist = Artist.find(params[:id])
   end
 
   def new
-    @artist = Artist.new
+    @artist = Artist.new 
   end
 
   def create
@@ -46,5 +48,9 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def song_count
+    @song_count = Artist.songs.all.count
   end
 end
